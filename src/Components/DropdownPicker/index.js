56649f023319ad4274d55964f8ecc58cce0bd140
vladimir.pereira <vladimir.pereira@codificar.com.br>
2020-11-12 09:30:24 -0300

    Fix ios picker error

--- src/Components/DropdownPicker/index.js
+++ src/Components/DropdownPicker/index.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-community/picker';
 import { useField } from '@unform/core';
 
 const DropdownPicker = (props) => {
-	const {label, datasource, name, onChange, stylesheet } = props;
+	const { label, datasource, name, onChange, stylesheet } = props;
 
 	const pickerRef = useRef(null);
 	const { fieldName, registerField, defaultValue, error } = useField(name);
 	const [option, setOption] = useState(defaultValue);
 
+	const [showPicker, setShowPicker] = useState(true);
+
 	useEffect(() => {
 		setOption(defaultValue);
-		pickerRef.current.value = defaultValue;
+		if (pickerRef.current) {
+			pickerRef.current.value = defaultValue;
+			setShowPicker(!showPicker)
+		}
 	}, [defaultValue]);
 
 	useEffect(() => {
@@ -51,7 +56,7 @@ const DropdownPicker = (props) => {
 
 	var formGroupStyle = stylesheet.select.normal;
 	var controlLabelStyle = stylesheet.controlLabel.normal;
-	var textboxStyle = stylesheet.textbox.normal;
+	var textboxStyle = Platform.OS === 'ios' ? {} : stylesheet.textbox.normal;
 	var errorBlockStyle = stylesheet.errorBlock;
 
 	if (!!error) {
@@ -61,15 +66,12 @@ const DropdownPicker = (props) => {
 		textboxViewStyle = stylesheet.textboxView.error;
 	}
 
-	var options = getDataSourceFields().map(({ value, text }) => (
-		<Picker.Item color="#3e3e3e" key={value} value={value} label={text} />
-	));
+	const showIOSPicker = () => {
+		setShowPicker(!showPicker);
+	}
 
-	return (
-		<View style={[styles.container, formGroupStyle]}>
-			{label && (
-				<Text style={[styles.label, controlLabelStyle]}>{label}</Text>
-			)}
+	const renderPicker = () => {
+		return (
 			<Picker
 				ref={pickerRef}
 				style={[styles.textInput, textboxStyle]}
@@ -82,27 +84,65 @@ const DropdownPicker = (props) => {
 					}
 				}}
 				{...props}>
-				{options}
+				{ renderOptions}
 				{!!error && (
 					<Text accessibilityLiveRegion="polite" style={[styles.error, errorBlockStyle]}>
 						{error}
 					</Text>
 				)}
 			</Picker>
-		</View>
+		)
+	}
+
+	const renderOptions = getDataSourceFields().map(({ value, text }) => {
+		return (<Picker.Item color="#3e3e3e" key={value} value={value} label={text} />)
+	});
+
+	const renderAndroid = () => {
+		return (
+			<View style={[styles.container, formGroupStyle]}>
+				{label && (
+					<Text style={[styles.label, controlLabelStyle]}>{label}</Text>
+				)}
+				{ renderPicker()}
+			</View>
+		)
+	}
+
+	const renderIOS = () => {
+		return (
+			<View style={[styles.container, formGroupStyle]}>
+				<TouchableOpacity onPress={showIOSPicker}>
+					{label && (
+						<Text style={[styles.label, controlLabelStyle]}>{label}</Text>
+					)}
+					<Text style={[styles.textInputIos, textboxStyle]}>{datasource[option]}</Text>
+				</TouchableOpacity>
+				{ showPicker && renderPicker()}
+			</View>
+		)
+	}
+
+	return (
+		Platform.OS === 'ios' ? renderIOS() : renderAndroid()
 	);
 };
 
 const styles = StyleSheet.create({
 	container: {
 		marginBottom: 15,
-		borderBottomWidth: StyleSheet.hairlineWidth,
+		borderBottomWidth: StyleSheet.hairlineWidth
 	},
 	label: {
 		fontSize: 13,
 	},
 	textInput: {
+		fontSize: 17
+	},
+	textInputIos: {
 		fontSize: 17,
+		paddingVertical: 5,
+		borderBottomWidth: StyleSheet.hairlineWidth
 	},
 	error: {
 		color: '#f00',
